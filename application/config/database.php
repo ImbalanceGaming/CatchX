7ad1080b83ea519e05d31e993e6e76882eb3b2c7
Christopher Pratt <chrispratt1985@gmail.com>
2015-03-09 01:48:05 +0000

    Changed database config to reflect azure hosting

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = 'test';
+$db['default']['username'] = 'catchx';
+$db['default']['password'] = 'l33tGaming';
+$db['default']['database'] = 'catchx';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
